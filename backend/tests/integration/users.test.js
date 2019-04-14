79e49f19ecda5b2b712053011d1407ab3c76437b
Liran Yehu <liran4200@gmail.com>
2019-04-14 16:27:17 +0300

    add all integration tests in users without auth yet

--- backend/tests/integration/users.test.js
+++ backend/tests/integration/users.test.js
@@ -2,12 +2,14 @@ const request = require('supertest');
 const {User, validate} = require('../../models/user');
 let server;
 describe('/api/users/', () => {
+
     beforeEach( () => {server = require('../../index');});
     afterEach( async () => {
         server.close();
         await User.remove({});            
     });
-    describe('GET /search/?keyWord={keyWord}&pageNumber={number}&pageSize={number}', () => {
+
+    describe('GET /search/?keyWord={keyWord}&pageNumber={pageNumber}&pageSize={pageSize}', () => {
         beforeEach( () => {server = require('../../index');});
         afterEach( async () => {
             server.close();
@@ -57,7 +59,7 @@ describe('/api/users/', () => {
             expect(res.body[0].lastName.toLowerCase()).toMatch(/yehudar/);
         });
 
-        it('should return message error because pageNumber or pageSize invalid', async () => {
+        it('should return 1 user with equal email in page 1', async () => {
             await User.collection.insertMany([
                 {
                     "firstName": "lirann",
@@ -72,11 +74,18 @@ describe('/api/users/', () => {
                     "password": "L2222"
                 }
             ]);
+            const res = await request(server).get('/api/users/search/?keyWord=liran@gmail.com&pageNumber=1&pageSize=2');
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(1);
+            expect(res.body[0].email).toMatch(/liran@gmail.com/);
+        });
+
+        it('should return 400 if pageNumber or pageSize invalid', async () => {
             const res = await request(server).get('/api/users/search/?keyWord=yehudar&pageNumber=0&pageSize=2');
             expect(res.status).toBe(400);
-            expect(res.text).toEqual("Invalid pageNumber or pageSize");
         });
     });
+
     describe('GET /:id', () => {
         it('should return user with the given id', async () => {
             const user = new User(
@@ -91,6 +100,100 @@ describe('/api/users/', () => {
             const res = await request(server).get('/api/users/'+ user._id);
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty('lirann', user.name);
+        });
+
+        it('should return 400 if the id is invalid', async () => {
+            const res = await request(server).get('/api/users/1');
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('GET /me', () => {
+        it('should return the current user who logged in', async () => {
+            const user =
+                {
+                    "firstName": "lirann",
+                    "lastName": "yehudar",
+                    "email": "liran4200@gmail.com",
+                    "password": "L2222"
+                };
+
+            const resWithToken = await request(server)
+                        .post('/api/users/register')
+                        .send(user);
+            
+            const res = await request(server)
+                        .get('/api/users/me')
+                        .set('x-auth-token', resWithToken.header['x-auth-token']);
+
+            expect(res.status).toBe(200);
+            expect(res.body._id).toBe(resWithToken.body._id);
+        });
+
+        it('should return 404 if user not found', async () => {
+            const user =
+                {
+                    "firstName": "lirann",
+                    "lastName": "yehudar",
+                    "email": "liran4200@gmail.com",
+                    "password": "L2222"
+                };
+
+            const resWithToken = await request(server)
+                        .post('/api/users/register')
+                        .send(user);
+            
+            await User.remove();
+            const res = await request(server)
+                        .get('/api/users/me')
+                        .set('x-auth-token', resWithToken.header['x-auth-token']);
+
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 401 if no token provided ', async () => {
+            const res = await request(server)
+                        .get('/api/users/me');
+
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 400 if invalid token', async () => {
+            const res = await request(server)
+                        .get('/api/users/me')
+                        .set("x-auth-token", "aaaaa");
+
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('POST /register', () => {
+        it('should return 400 if the user already exists', async () => {
+            const user = new User(
+                {
+                    "firstName": "lirann",
+                    "lastName": "yehudar",
+                    "email": "liran4200@gmail.com",
+                    "password": "L2222"
+                });
+            await user.save();
+
+            const res = await request(server).post('/api/users/register').send(user);
+            expect(res.status).toBe(400);
+        });
+
+        it('should save the user in DB', async () => {
+            const user =
+                {
+                    "firstName": "lirann",
+                    "lastName": "yehudar",
+                    "email": "liran4200@gmail.com",
+                    "password": "L2222"
+                };
+
+            const res = await request(server).post('/api/users/register').send(user);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('_id');
         });
     });
 });
