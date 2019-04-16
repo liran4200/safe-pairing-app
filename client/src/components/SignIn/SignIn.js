f93b70cc2b06ed107fde1865a887abc2eef0d13d
Liran Yehudar <liran4200@gmail.com>
2019-04-16 23:19:17 +0300

    add client directory template

---
+++ client/src/components/SignIn/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from './node_modules/reacte_modules/react';
+import { BrowserRouter as Router, Route, Link } from './node_modules/react-router-domeact-router-dom';
+
+class SignIn extends Component {
+  render() {
+    return (
+        <div>
+            SignIn Page
+        </div>
+    );
+  }
+}
+
+export default SignIn;
