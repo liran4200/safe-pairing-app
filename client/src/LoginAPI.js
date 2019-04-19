007372a2cff6e32cbcb56821272ea027da99caff
Liran Yehudar <liran4200@gmail.com>
2019-04-19 16:47:08 +0300

    add users and login api handlers

---
+++ client/src/LoginAPI.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:4444/api";
+
+export const login = async (credentials) => {
+    try{
+        const url = BASE_URL + '/login'
+        const res = await axios.post(url, credentials, {
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(res.data);
+    }
+    catch(error) {
+        console.error(error);
+    }
+} 
